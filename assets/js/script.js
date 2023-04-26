d7eff117df744c63bfb714541b03a754404d8e67
Marissa T <marissalynnthompson@gmail.com>
2023-04-26 00:22:15 -0600

    updated JavaScript and HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 var timeDisplayEl = $("#time-display");
 
 // handle displaying the time
@@ -13,32 +9,32 @@ function displayTime() {
 displayTime();
 setInterval(displayTime, 1000);
 
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
+$(function () {
 
-// var userInputEl = $('#user-input');
-// var saveBtn = $('#save-button');
+  
+  //function to save user data typed into the text area
 
-// saveBtn.addEventListent("click", function(event)
-// {
-//   event.preventDefalut();
-//   var userInput = document.querySelector("#user-input").value;
-
-//   localStorage.setItem("userInput", userInput);
-// });
+  $('.saveBtn').click(function() {
+    var parentID = $(this).parent().closest('div').attr('ID');
+    var newTask = $(this).siblings('.description').val();
+    localStorage.setItem(parentID, newTask);
+  });
+  //run a function to display locally stored user tasks on application 
+  $('.time-block').each(function() {
+    var storedTask = localStorage.getItem($(this).attr('ID'));
+    $(this).find("textarea").append(storedTask);
+  });
+  //run a function to apply css color based on hour
+  $('.time-block').each(function() {
+    var hour = ("hour-" + dayjs().format("HH"));
+    if ($(this).attr('ID') == hour) {
+      $(this).addClass('present');
+    }
+    else if ($(this).attr('ID') < hour) {
+      $(this).addClass('past');
+    }
+    else {
+      $(this).addClass('future');
+    }
+  });
+});
